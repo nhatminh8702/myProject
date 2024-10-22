a858f90a431eeb4c92d56b05afabf90b122c21a8
nhatminh <nhatminh8702@gmail.com>
2024-10-22 17:25:50 +0700

    commit things

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './heroes-type';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'WindStorm',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
